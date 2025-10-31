ed51a6d8c5dd6ff138b188cb302544eb56a7484f
Bui Thanh Tung <buithanhtungaabb@gmail.com>
2025-11-01 04:10:55 +0700

    b

--- src/main/java/vn/iotstar/services/ProductImageService.java
+++ src/main/java/vn/iotstar/services/ProductImageService.java
@@ -48,8 +48,10 @@ public class ProductImageService {
         EntityManager em = JPAConfig.getEntityManager();
         try {
             return (String) em.createNativeQuery(
-                "SELECT TOP 1 image_url FROM Product_Image " +
-                "WHERE product_id = ? AND is_thumbnail = 1")
+                "SELECT image_url\r\n"
+                + "FROM Product_Image\r\n"
+                + "WHERE product_id = ? AND is_thumbnail = true\r\n"
+                + "LIMIT 1;")
                 .setParameter(1, productId)
                 .getResultStream()
                 .findFirst()
