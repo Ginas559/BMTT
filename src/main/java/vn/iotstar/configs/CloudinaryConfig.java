d83206c773c4f25586403c93343856c4f390ccdb
Bui Thanh Tung <buithanhtungaabb@gmail.com>
2025-11-01 03:34:15 +0700

    cau nguyen

--- src/main/java/vn/iotstar/configs/CloudinaryConfig.java
+++ src/main/java/vn/iotstar/configs/CloudinaryConfig.java
@@ -2,16 +2,19 @@ package vn.iotstar.configs;
 
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
+import io.github.cdimascio.dotenv.Dotenv;
 
 public class CloudinaryConfig {
+
+    private static final Dotenv dotenv = Dotenv.load();
+
     private static final Cloudinary cloudinary = new Cloudinary(ObjectUtils.asMap(
-        "cloud_name", System.getenv("CLOUDINARY_NAME"),
-        "api_key", System.getenv("CLOUDINARY_API_KEY"),
-        "api_secret", System.getenv("CLOUDINARY_API_SECRET"),
-        "secure", true
+        "cloud_name", dotenv.get("CLOUDINARY_CLOUD_NAME"),
+        "api_key", dotenv.get("CLOUDINARY_API_KEY"),
+        "api_secret", dotenv.get("CLOUDINARY_API_SECRET")
     ));
 
-    public static Cloudinary getInstance() {
+    public static Cloudinary getCloudinary() {
         return cloudinary;
     }
 }
